40af04434702f1ce350464e2d314cee898c7adb4
KuessiAnsan <kuessiansandjiyehoue@gmail.com>
2026-04-04 10:26:37 +0200

    Fixes

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const AUTHOR_NAME = 'Manuel D.';
 export const AUTHOR_BIO = 'Engineering \u{1F6E0}\u{FE0F} \u007E Scalable Infrastructures, Cloud Computing \u0026 Machine Learning \u{1F9EE}';
 export const AUTHOR_IMAGE = '/images/author.jpg';
 
-export const YOUTUBE_CHANNEL_ID = 'UCbB2VUG0SGPFHlWxvkDE63g';
+export const YOUTUBE_CHANNEL_ID = 'UCuvW7z1K0EzT_rsf50A6Hqg';
 
 export const SOCIAL_LINKS = {
   youtube: 'https://www.youtube.com/@engwmanuel',
